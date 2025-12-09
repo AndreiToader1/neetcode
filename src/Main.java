b5896a15135ae64f11fe671dc3c45289cb3c6da2
claudiapapp <tdandrey94@gmail.com>
2025-12-09 20:57:26 +0200

    add two sum solution

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import ValidAnagram.Solution;
+import TwoSum.Solution;
 
 public class Main {
     public static void main(String[] args) {
         var solution = new Solution();
-        var s = "jap";
-        var t = "ja";
-        System.out.println(solution.isAnagram(s,t));
+        var nums = new int[] {5,5};
+        var target = 10;
+
+        var result = solution.twoSum(nums, target);
+        for (var number: result) {
+            System.out.println(number);
+        }
     }
 }

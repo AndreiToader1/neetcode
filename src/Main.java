6cc768e04dbf33fa0dbe9bd14afe1b1cd487f961
claudiapapp <tdandrey94@gmail.com>
2025-12-19 21:44:00 +0200

    added top k frequent solution

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import ValidParentheses.Solution;
+import topkfrequent.Solution;
 
 public class Main {
     public static void main(String[] args) {
         var solution = new Solution();
-        var parentheses = "[";
-        var result = solution.isValid(parentheses);
-        System.out.println(result);
+        var nums = new int[] {7, 7};
+        var k = 1;
+
+        var result = solution.topKFrequent(nums, k);
+        for (int j : result) {
+            System.out.println(j);
+        }
     }
 }

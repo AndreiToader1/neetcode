88857c97e11437ef2773dc3edd61cd861d023826
claudiapapp <tdandrey94@gmail.com>
2025-12-23 22:29:04 +0200

    added encode and decode string

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-import binarysearch.Solution;
+import encodedecodestring.Solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        var list = new ArrayList<>(Arrays.asList("we","say",":","yes"));
         var solution = new Solution();
-        var result = solution.search(new int[] {-1,0,2,4,6,8}, 4);
-        System.out.println(result);
+        var encodedResult = solution.encode(list);
+        var decodedResult = solution.decode(encodedResult);
+        for (var result: decodedResult) {
+            System.out.println(result);
+        }
     }
 }

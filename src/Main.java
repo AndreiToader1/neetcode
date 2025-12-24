46fec3f3940be4aad5f694c3d4e8bef0dc23cca5
claudiapapp <tdandrey94@gmail.com>
2025-12-24 12:40:16 +0200

    added valid sudoku problem

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
-import encodedecodestring.Solution;
+import validsudoku.Solution;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new ArrayList<>(Arrays.asList("we","say",":","yes"));
+        var board = new char[][] {
+                {'1','2','.','.','3','.','.','.','.'},
+                {'4','.','.','5','.','.','.','.','.'},
+                {'.','9','1','.','.','.','.','.','3'},
+                {'5','.','.','.','6','.','.','.','4'},
+                {'.','.','.','8','.','3','.','.','5'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','.','.','.','.','.','2','.','.'},
+                {'.','.','.','4','1','9','.','.','8'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
         var solution = new Solution();
-        var encodedResult = solution.encode(list);
-        var decodedResult = solution.decode(encodedResult);
-        for (var result: decodedResult) {
-            System.out.println(result);
-        }
+        var result = solution.isValidSudoku(board);
+        System.out.print(result);
     }
 }
---
+++ src/validsudoku/Solution.java
@@ -0,0 +1,43 @@
+package validsudoku;
+
+public class Solution {
+    private static final int BOARD_SIZE = 9;
+    private final short[][] lineFrequency;
+    private final short[][] columnsFrequency;
+    private final short[][] subBoxesFrequency;
+
+    public Solution() {
+        lineFrequency = new short[BOARD_SIZE][BOARD_SIZE];
+        columnsFrequency = new short[BOARD_SIZE][BOARD_SIZE];
+        subBoxesFrequency = new short[BOARD_SIZE][BOARD_SIZE];
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for (int line = 0; line < BOARD_SIZE; line++) {
+            for (int column = 0; column < BOARD_SIZE; column++) {
+                if (board[line][column] == '.') {
+                    continue;
+                }
+
+                var cellValue = Character.getNumericValue(board[line][column]);
+                lineFrequency[line][cellValue-1]++;
+                columnsFrequency[column][cellValue-1]++;
+
+                int subBoxFrequencyIndex = line/3 * 3  + column / 3 ;
+                subBoxesFrequency[subBoxFrequencyIndex][cellValue-1]++;
+            }
+        }
+        return isValidFrequency(lineFrequency) && isValidFrequency(columnsFrequency) && isValidFrequency(subBoxesFrequency);
+    }
+
+    private static boolean isValidFrequency(short[][] frequencyBoard) {
+        for (int line = 0; line < BOARD_SIZE; line++) {
+            for (int column = 0; column < BOARD_SIZE; column++) {
+                if (frequencyBoard[line][column] > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}

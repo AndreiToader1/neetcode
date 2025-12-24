66f762e586d614618b8ea35eefbc29076500c121
claudiapapp <tdandrey94@gmail.com>
2025-12-24 13:04:47 +0200

    added longest consecutive sequence

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
-import validsudoku.Solution;
+import longestconsecutivesequence.Solution;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var board = new char[][] {
-                {'1','2','.','.','3','.','.','.','.'},
-                {'4','.','.','5','.','.','.','.','.'},
-                {'.','9','1','.','.','.','.','.','3'},
-                {'5','.','.','.','6','.','.','.','4'},
-                {'.','.','.','8','.','3','.','.','5'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','.','.','.','.','.','2','.','.'},
-                {'.','.','.','4','1','9','.','.','8'},
-                {'.','.','.','.','8','.','.','7','9'}
-        };
+        var nums = new int[] {0,3,2,5,4,6,1,1};
         var solution = new Solution();
-        var result = solution.isValidSudoku(board);
+        var result = solution.longestConsecutive(nums);
         System.out.print(result);
     }
 }

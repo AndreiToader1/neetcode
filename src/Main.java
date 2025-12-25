85ff02185ce0899359d5563a5fd6744caa3a5080
claudiapapp <tdandrey94@gmail.com>
2025-12-25 15:09:31 +0200

    added container with most water

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import threesum.Solution;
+import containerwithmostwater.Solution;
 
 
 public class Main {
     public static void main(String[] args) {
-        var nums = new int[] {-1,0,1,2,-1,-4,-2,-3,3,0,4};
+        var heights = new int[] {2,2,2};
         var solution = new Solution();
-        var result = solution.threeSum(nums);
-        System.out.print(result.toString());
+        var result = solution.maxArea(heights);
+        System.out.print(result);
     }
 }

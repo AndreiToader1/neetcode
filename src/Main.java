f8aec2a8a86f4d4d15c59e6032a25323122a4938
claudiapapp <tdandrey94@gmail.com>
2025-12-25 12:50:56 +0200

    added three sum solution

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import longestconsecutivesequence.Solution;
+import threesum.Solution;
 
 
 public class Main {
     public static void main(String[] args) {
-        var nums = new int[] {0,3,2,5,4,6,1,1};
+        var nums = new int[] {-1,0,1,2,-1,-4,-2,-3,3,0,4};
         var solution = new Solution();
-        var result = solution.longestConsecutive(nums);
-        System.out.print(result);
+        var result = solution.threeSum(nums);
+        System.out.print(result.toString());
     }
 }

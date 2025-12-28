cf24ce254ab14286b2a97a4e53f93a2413ebcb48
claudiapapp <tdandrey94@gmail.com>
2025-12-28 21:35:35 +0200

    fix solution

--- src/reverselinkedlist/Solution.java
+++ src/reverselinkedlist/Solution.java
@@ -4,15 +4,16 @@ import utils.ListNode;
 
 public class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode currentNode = head.next;
-        ListNode previousNode = head;
-        while (currentNode != null) {
-            ListNode nextNode = currentNode.next;
-            currentNode.next = previousNode;
-            previousNode = currentNode;
-            currentNode = nextNode;
-        }
+       ListNode prevNode = null;
+       ListNode currentNode = head;
+       while (currentNode != null) {
+           ListNode nextNode = currentNode.next;
+           currentNode.next = prevNode;
 
-        return currentNode;
+           prevNode = currentNode;
+           currentNode = nextNode;
+       }
+
+       return prevNode;
     }
 }

b7a66e7f28aa8a437dd125e19482b46b4a4b8038
claudiapapp <tdandrey94@gmail.com>
2025-12-31 14:51:17 +0200

    added diameter of binary tree solution

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import maximumdepthbinarytree.Solution;
+import diameterofbinarytree.Solution;
 import utils.binarytrees.BinaryTreesUtils;
-import utils.binarytrees.TreeNode;
 
 
 public class Main {
     public static void main(String[] args) {
         var solution = new Solution();
-        var values = new Integer[] {3,2,1};
+        var values = new Integer[] {1,null, 2,3,4,5};
         var rootNode = BinaryTreesUtils.constructBinaryTree(values);
-        int maxDepth = solution.maxDepth(rootNode);
-        System.out.println(maxDepth);
+        int treeDiameter = solution.diameterOfBinaryTree(rootNode);
+        System.out.println(treeDiameter);
     }
 }

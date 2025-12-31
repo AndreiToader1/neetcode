1a5388f7188d86e42e1676211666703cb54bee39
claudiapapp <tdandrey94@gmail.com>
2025-12-31 15:51:47 +0200

    added balanced binary tree solution

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import diameterofbinarytree.Solution;
+import balancedbinarytree.Solution;
 import utils.binarytrees.BinaryTreesUtils;
 
 
 public class Main {
     public static void main(String[] args) {
         var solution = new Solution();
-        var values = new Integer[] {1,null, 2,3,4,5};
+        var values = new Integer[]{};
         var rootNode = BinaryTreesUtils.constructBinaryTree(values);
-        int treeDiameter = solution.diameterOfBinaryTree(rootNode);
-        System.out.println(treeDiameter);
+        boolean isBalanced = solution.isBalanced(rootNode);
+        System.out.println(isBalanced);
     }
 }
---
+++ src/balancedbinarytree/Solution.java
@@ -0,0 +1,30 @@
+package balancedbinarytree;
+
+import utils.binarytrees.TreeNode;
+
+public class Solution {
+    private boolean balanced;
+
+    public Solution() {
+        balanced = true;
+    }
+    public boolean isBalanced(TreeNode root) {
+        treeHeight(root);
+        return balanced;
+    }
+
+    private int treeHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = treeHeight(root.left);
+        int rightHeight = treeHeight(root.right);
+
+        if (Math.abs(leftHeight- rightHeight) > 1) {
+            balanced= false;
+        }
+
+        return 1+ Math.max(leftHeight, rightHeight);
+    }
+}
